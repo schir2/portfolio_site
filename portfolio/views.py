a550897852827c7cc0c3b1c77478119c53dbcf48
Marek Schir <schir2@msn.com>
2020-03-20 02:54:44 -0400

    Added background images.

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,3 +6,28 @@ from run import app
 @app.route('/')
 def index():
     return render_template('portfolio/index.html')
+
+
+@app.route('/about_me')
+def about_me():
+    return render_template('portfolio/about_me.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('portfolio/blog.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('portfolio/projects.html')
+
+
+@app.route('/resume')
+def resume():
+    return render_template('portfolio/resume.html')
+
+
+@app.route('/contact_ma')
+def contact_me():
+    return render_template('portfolio/contact_me.html')
